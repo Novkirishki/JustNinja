43d89b78d15a40c26215a3d2ddde712e1889410c
Nikolai Novkirishki <novkirishki94@gmail.com>
2016-01-31 16:50:18 +0200

    Added ground

--- Just Ninja/Just Ninja/GameScene.swift
+++ Just Ninja/Just Ninja/GameScene.swift
@@ -9,12 +9,19 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var ground: Ground!
+    
     override func didMoveToView(view: SKView) {
-        backgroundColor = UIColor.redColor()
+        backgroundColor = UIColor(red: 159.0/255.0, green: 201.0/255.0, blue: 244.0/255.0, alpha: 1.00)
+        
+        ground = Ground(size: CGSizeMake(view.frame.width, 20))
+        ground.position = CGPointMake(0, view.frame.size.height/2)
+        addChild(ground)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
+       ground.start()
     }
    
     override func update(currentTime: CFTimeInterval) {
---
+++ Just Ninja/Just Ninja/Ground.swift
@@ -0,0 +1,47 @@
+//
+//  Ground.swift
+//  Just Ninja
+//
+//  Created by Nikolai Novkirishki on 1/31/16.
+//  Copyright © 2016 Nikolai Novkirishki. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Ground: SKSpriteNode {
+    let NUMBER_OF_SEGMENTS = 20
+    let COLOR_ONE = UIColor(red: 88.0/255.0, green: 148.0/255.0, blue: 87.0/255.0, alpha: 1.00)
+    let COLOR_TWO = UIColor(red: 120.0/255.0, green: 195.0/255.0, blue: 118.0/255.0, alpha: 1.00)
+    
+    init(size: CGSize) {
+        super.init(texture: nil, color: UIColor.brownColor(), size: CGSizeMake(size.width*2, size.height))
+        anchorPoint = CGPointMake(0, 0.5)
+        
+        for var i = 0; i < NUMBER_OF_SEGMENTS; i++ {
+            var segmentColor: UIColor!
+            if i % 2 == 0 {
+                segmentColor = COLOR_ONE
+            } else {
+                segmentColor = COLOR_TWO
+            }
+            
+            let segment = SKSpriteNode(color: segmentColor, size: CGSizeMake(self.size.width / CGFloat(NUMBER_OF_SEGMENTS), size.height))
+            segment.anchorPoint = CGPointMake(0.0, 0.5)
+            segment.position = CGPointMake(CGFloat(i)*segment.size.width, 0)
+            addChild(segment)
+        }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func start() {
+        let moveLeft = SKAction.moveByX(-frame.size.width/2, y: 0, duration: 1.0)
+        let resetPosition = SKAction.moveToX(0, duration: 0)
+        let moveSequence = SKAction.sequence([moveLeft, resetPosition])
+        
+        runAction(SKAction.repeatActionForever(moveSequence))
+    }
+}
