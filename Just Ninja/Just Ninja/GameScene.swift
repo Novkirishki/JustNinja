dd3a1f493239f229670e7b3c8ffb53f3932fd060
Nikolai Novkirishki <novkirishki94@gmail.com>
2016-01-31 16:01:47 +0200

    Added game project

---
+++ Just Ninja/Just Ninja/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  Just Ninja
+//
+//  Created by Nikolai Novkirishki on 1/31/16.
+//  Copyright (c) 2016 Nikolai Novkirishki. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        backgroundColor = UIColor.redColor()
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       /* Called when a touch begins */
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
---
+++ Just Ninja/Just Ninja/GameViewController.swift
@@ -0,0 +1,52 @@
+//
+//  GameViewController.swift
+//  Just Ninja
+//
+//  Created by Nikolai Novkirishki on 1/31/16.
+//  Copyright (c) 2016 Nikolai Novkirishki. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    var scene: GameScene!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Configure the view
+        let skView = view as! SKView
+        skView.multipleTouchEnabled = false
+        
+        // Create and confugure the scene
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        
+        // Present the scene
+        skView.presentScene(scene)
+    }
+
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+            return .AllButUpsideDown
+        } else {
+            return .All
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
+	
