4e7ab0e0d0466ca881134e558be68c7926275103
Nikolai Novkirishki <novkirishki94@gmail.com>
2016-01-31 22:57:53 +0200

    Added hero with breathing, running and flip animation

---
+++ Just Ninja/Just Ninja/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Just Ninja
+//
+//  Created by Nikolai Novkirishki on 1/31/16.
+//  Copyright © 2016 Nikolai Novkirishki. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let GROUND_HEIGHT: CGFloat = 20.0
--- Just Ninja/Just Ninja/GameScene.swift
+++ Just Ninja/Just Ninja/GameScene.swift
@@ -11,17 +11,36 @@ import SpriteKit
 class GameScene: SKScene {
     
     var ground: Ground!
+    var ninja: Ninja!
+    
+    var isGameStarted = false
     
     override func didMoveToView(view: SKView) {
         backgroundColor = UIColor(red: 159.0/255.0, green: 201.0/255.0, blue: 244.0/255.0, alpha: 1.00)
         
-        ground = Ground(size: CGSizeMake(view.frame.width, 20))
+        ground = Ground(size: CGSizeMake(view.frame.width, GROUND_HEIGHT))
         ground.position = CGPointMake(0, view.frame.size.height/2)
         addChild(ground)
+        
+        ninja = Ninja()
+        ninja.position = CGPointMake(70, ground.position.y + ground.frame.size.height/2 + ninja.frame.size.height/2)
+        addChild(ninja)
+        ninja.breathe()
+    }
+    
+    func start() {
+        isGameStarted = true
+        ninja.stop()
+        ninja.startRunning()
+        ground.start()
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       ground.start()
+        if !isGameStarted {
+            start()
+        } else {
+            ninja.flip()
+        }
     }
    
     override func update(currentTime: CFTimeInterval) {
